e06db9a9a7b4422f404e5fc4f54bb44c8ae8a754
MoiChuu <simplelove1210@gmail.com>
2026-02-07 03:59:23 +0800

    新增彩蛋與 console 提示，更新版權，刪除部分連結
    新增 text/human 與 console warning 彩蛋，修正版權宣告，刪除部分連結

---
+++ js/console-warning.js
@@ -0,0 +1,30 @@
+// console-warning.js
+
+console.log(
+  '%c唉唷？你如果發現這一層，那你一定是工程師囉？',
+  'color:#ff9800; font-size:18px; font-weight:bold;'
+);
+
+console.log(
+  '%c歡迎工程師幫我debug 😏',
+  'color:#FFF; font-size:13px;'
+);
+
+console.log(
+`%c
+(\\_/)
+( •_•)
+ / >🍪
+`,
+  'color:#e70088; font-size:12px;'
+);
+
+console.log(
+  '%c⚠️ 提醒您',
+  'color:#fd0; font-size:16px; font-weight:bold;'
+);
+
+console.log(
+  '%c本網站所有的圖像與文章均受到著作財產權的保護。\n著作權人有權禁止他人未經授權的任何行為。',
+  'color:#d32f2f; font-size:14px;'
+);
